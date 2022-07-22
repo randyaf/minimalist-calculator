2c9a0b338d8cd3fb483767b01d59fe278409251a
randyaf <randyaf29@gmail.com>
2022-07-22 23:31:28 +0700

    Make the button to be able to store data into array
    - inputted number now stored into array
    - inputted operator also stored into array
    - the functionality to calculate the  operation is not added yet

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 let result = document.querySelector(".calculation-display");
 
 let numberButtonContainer = document.querySelector(".number-button-container");
+let operatorContainer = document.querySelector(".operator-container");
+
+let inputtedNumber = [];
+let inputtedOperator = [];
+
+let isOperatorPressed = false;
+let isNumberButtonPressed = false;
+let operator = "";
 
 numberButtonContainer.addEventListener("click", event => {
     if(event.target.matches("button")) {
+
+        if (isNumberButtonPressed === false && operator !== "") {
+            inputtedOperator.push(operator);
+        } 
+        isNumberButtonPressed = true;
+
         console.log(event.target.getAttribute("value"));
         if(event.target.getAttribute("value") === ".") {
             if ( result.innerText.includes(".")) {
@@ -16,5 +30,68 @@ numberButtonContainer.addEventListener("click", event => {
             result.innerText = result.innerText
             .concat(event.target.getAttribute("value"));
         }
+        isOperatorPressed = false;
     }
-})
+});
+
+
+operatorContainer.addEventListener("click", event => {
+    // TODO HERE
+    if (isOperatorPressed === false) inputtedNumber.push(Number(result.innerText));
+
+    result.innerText = "";
+
+    if(event.target.matches("button")) {
+        isOperatorPressed = true;
+        console.log(event.target.getAttribute("value"));
+        if(event.target.getAttribute("value") === "*") {
+            operator = "*";
+        } else if (event.target.getAttribute("value") === "/") {
+            operator = "/";
+        } else if (event.target.getAttribute("value") === "+") {
+            operator = "+";
+        } else if (event.target.getAttribute("value") === "-") {
+            operator = "-";
+        }
+    }
+    console.log(inputtedNumber);
+    console.log(inputtedOperator);
+    isNumberButtonPressed = false;
+});
+
+
+function addition(firstNumber, secondNumber) {
+    if(typeof firstNumber === Number && typeof secondNumber === number) {
+        return firstNumber + secondNumber;
+    } else {
+        return "NaN";
+    }
+}
+
+function substraction(firstNumber, secondNumber) {
+    if(typeof firstNumber == number && typeof secondNumber == number) {
+        return firstNumber - secondNumber;
+    } else {
+        return "NaN";
+    }
+}
+
+function multiplication(firstNumber, secondNumber) {
+    if(typeof firstNumber == "number" && typeof secondNumber == "number") {
+        return firstNumber * secondNumber;
+    } else {
+        return "NaN";
+    }
+}
+
+function division(firstNumber, secondNumber) {
+    if(typeof firstNumber == number && typeof secondNumber == number) {
+        return firstNumber / secondNumber;
+    } else {
+        return "NaN";
+    }
+}
+
+function calculate() {
+    
+}
